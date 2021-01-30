429316ece5189ca4f510d5289c35b54cff31a62b
Armando <armandozabala@gmail.com>
2021-01-30 15:53:37 -0500

    first commit react-crud-task-firestre

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDUuh3wM-ZgBMTFT5OHWyDIuGcfD88cRRg",
+    authDomain: "detector-bff46.firebaseapp.com",
+    databaseURL: "https://detector-bff46.firebaseio.com",
+    projectId: "detector-bff46",
+    storageBucket: "detector-bff46.appspot.com",
+    messagingSenderId: "756969306515",
+    appId: "1:756969306515:web:88072d1858018aa1b69066"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  export { firebase }
